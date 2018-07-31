48957c990b6e5d2c6f08c6a62817026c08ca9583
Bryan Lim <bryan.lim@procore.com>
2018-07-31 15:43:05 -0700

    Skeleton spec file

---
+++ 04_Rails_Controller_and_View/spec/rails_controller_and_view_specs.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+# ADD TO README LATER:
+# For this challenge you must create the Rails controller for
+# the ItemHistory model. Use 'rails g controller' to create 
+# a controller called item_histories_controller with index and show actions.
+# 
+# Create a .html.erb file for both index and show 
+# (ignore this if you created them already with the rails g controller method).
+# In index and show, have the page display "All my item histories!" and
+# "Just one item history!" respectively.
+
+describe "Checklist ItemHistory Controller" do
+  it 'test 1' do 
+  end
+  it 'test 2' do 
+  end
+  it 'test 3' do 
+  end
+  it 'test 4' do 
+  end
+end
+
+describe "Checklist ItemHistory View" do
+  it 'test 1' do 
+  end
+  it 'test 2' do 
+  end
+  it 'test 3' do 
+  end
+  it 'test 4' do 
+  end
+  it 'test 5' do 
+  end
+end
