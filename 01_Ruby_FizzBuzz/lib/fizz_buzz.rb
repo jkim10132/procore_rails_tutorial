9906cfda7555d3200b3b18f6d6f566fa8fc4fd38
jkim10132 <justin.kim@procore.com>
2018-08-02 10:36:13 -0700

    Finished Fizz

--- 01_Ruby_FizzBuzz/lib/fizz_buzz.rb
+++ 01_Ruby_FizzBuzz/lib/fizz_buzz.rb
@@ -3,6 +3,15 @@ def fizzbuzz_helper(number)
   #Return 'Fizz' for multiples of 3
   #Return 'Buzz for multiples of 5
   #Return 'FizzBuzz' for multiples of 3 and 5
+  if number % 5 == 0 and number % 3 == 0
+        return "FizzBuzz"
+      elsif number % 5 == 0
+        return "Buzz"
+      elsif number % 3 == 0
+        return "Fizz"
+      else
+        return number
+      end
 end
 
 
