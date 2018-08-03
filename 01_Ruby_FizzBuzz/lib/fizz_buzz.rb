45149056ae053aab8bdb9bf597c3a55c6ad3fcbe
jkim10132 <justin.kim@procore.com>
2018-08-03 10:54:32 -0700

    test

--- 01_Ruby_FizzBuzz/lib/fizz_buzz.rb
+++ 01_Ruby_FizzBuzz/lib/fizz_buzz.rb
@@ -3,10 +3,20 @@ def fizzbuzz_helper(number)
   #Return 'Fizz' for multiples of 3
   #Return 'Buzz for multiples of 5
   #Return 'FizzBuzz' for multiples of 3 and 5
+  if (number % 15 == 0)
+    "FizzBuzz"
+  elsif (number % 5 == 0)
+    "Buzz"
+  elsif (number % 3 == 0)
+    "Fizz"
+  else
+    number
+  end
 end
 
 
 def fizz_buzz(max)
+  (1..max).map {|i| fizzbuzz_helper(i)} 
   #TODO: Return an array of numbers converted to fizzbuzz
   # EX: fizz_buzz(5) = [1,2,"Fizz",4,"Buzz"]
 end
